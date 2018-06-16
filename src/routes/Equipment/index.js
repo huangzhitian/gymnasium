ec3333e67409ac621527b3b803ef5ed44019f1f3
sillyhong <1175166300@qq.com>
2018-06-16 18:30:17 +0800

    first commit

---
+++ src/routes/Equipment/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class EquipMent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>这里是器材管理</div>;
+  }
+}
+export default EquipMent;
---
+++ src/routes/Match/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Match extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>这里是赛事管理</div>;
+  }
+}
+export default Match;
